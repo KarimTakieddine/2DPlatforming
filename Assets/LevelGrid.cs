9b0248e5ba9eb49982571d6331d908881c2878ef
MounirTakieddine <MounirTakieddine@bitbucket.org>
2018-01-15 03:49:10 +0000

    Initial repository upload. Implemented LevelGrid component exposing high-level dimensions and line thickness parameters in the inspector. Created basic ShaderLab passthrough shader with fragment code animating color intensity

---
+++ Assets/LevelGrid.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class LevelGrid : MonoBehaviour
+{
+    public MeshFilter   Filter      { get; private set; }
+    public Mesh         Geometry    { get; private set; }
+
+    public Vector3 Origin;
+    public int GridWidth, GridHeight;
+    public int CellSize;
+
+    [Range(0.0f, 1.0f)]
+    public float LineThickness;
+
+	private void Update()
+    {
+        Filter      = GetComponent<MeshFilter>();
+        Geometry    = new Mesh();
+        InitializeGridGeometry();
+        Filter.sharedMesh = Geometry;
+    }
+
+    public void InitializeSurfaceGeometry()
+    {
+        int widthPlusOne    = GridWidth + 1;
+        int heightPlusOne   = GridHeight + 1;
+        Vector3[] vertices  = new Vector3[widthPlusOne * heightPlusOne];
+
+        for (int i = 0; i < heightPlusOne; ++i)
+        {
+            for (int j = 0; j < widthPlusOne; ++j)
+            {
+                vertices[i * widthPlusOne + j] = Origin + new Vector3(j, i);
+            }
+        }
+
+        int[] triangles     = new int[GridWidth * GridHeight * 6];
+        int triangleIndex   = 0;
+
+        for (int i = 0; i < GridHeight; ++i)
+        {
+            for (int j  = 0; j < GridWidth; ++j)
+            {
+                int topLeftIndex            = i * widthPlusOne + j;
+                int topRightIndex           = topLeftIndex + 1;
+                int bottomRightIndex        = topRightIndex + widthPlusOne;
+                int bottomLeftIndex         = bottomRightIndex - 1;
+
+                triangles[triangleIndex++]  = bottomLeftIndex;
+                triangles[triangleIndex++]  = bottomRightIndex;
+                triangles[triangleIndex++]  = topRightIndex;
+
+                triangles[triangleIndex++]  = bottomLeftIndex;
+                triangles[triangleIndex++]  = topRightIndex;
+                triangles[triangleIndex++]  = topLeftIndex;
+            }
+        }
+
+        Geometry.Clear();
+        Geometry.vertices   = vertices;
+        Geometry.triangles  = triangles;
+    }
+
+    public void InitializeGridGeometry()
+    {
+        int widthPlusOne    = GridWidth + 1;
+        int heightPlusOne   = GridHeight + 1;
+        Vector3[] vertices  = new Vector3[widthPlusOne * heightPlusOne * 4 + 1];
+        int[] triangles     = new int[GridWidth * GridHeight * 6 * 4];
+        int vertexIndex     = 0;
+        int triangleIndex   = 0;
+
+
+        for (int i = 0; i < heightPlusOne; ++i)
+        {
+            for (int j = 0; j < widthPlusOne; ++j)
+            {
+                vertices[vertexIndex++] = new Vector3(j, i);
+            }
+
+            for (int j = 0; j < widthPlusOne; ++j)
+            {
+                vertices[vertexIndex++] = new Vector3(j, i + LineThickness);
+            }
+        }
+
+        for (int i = 0; i < heightPlusOne * 2; i += 2)
+        {
+            for (int j  = 0; j < GridWidth; ++j)
+            {
+                int topLeftIndex            = i * widthPlusOne + j;
+                int topRightIndex           = topLeftIndex + 1;
+                int bottomLeftIndex         = topLeftIndex + widthPlusOne;
+                int bottomRightIndex        = bottomLeftIndex + 1;
+
+                triangles[triangleIndex++]  = bottomLeftIndex;
+                triangles[triangleIndex++]  = bottomRightIndex;
+                triangles[triangleIndex++]  = topRightIndex;
+
+                triangles[triangleIndex++]  = bottomLeftIndex;
+                triangles[triangleIndex++]  = topRightIndex;
+                triangles[triangleIndex++]  = topLeftIndex;
+            }
+        }
+
+        int horizontalMaximum = vertexIndex;
+
+        Debug.Log(horizontalMaximum);
+
+        for (int i = 0; i < widthPlusOne; ++i)
+        {
+            for (int j = 0; j < heightPlusOne; ++j)
+            {
+                vertices[vertexIndex++] = new Vector3(i, j);
+            }
+
+            for (int j = 0; j < heightPlusOne; ++j)
+            {
+                vertices[vertexIndex++] = new Vector3(i + LineThickness, j);
+            }
+        }
+
+        for (int i = 0; i < widthPlusOne * 2; i += 2)
+        {
+            for (int j = 0; j < GridHeight; ++j)
+            {
+                
+                int bottomLeftIndex = horizontalMaximum + (i * heightPlusOne) + j;
+                int bottomRightIndex = bottomLeftIndex + heightPlusOne;
+                int topLeftIndex = bottomLeftIndex + 1;
+                int topRightIndex = topLeftIndex + heightPlusOne;
+
+                triangles[triangleIndex++] = bottomLeftIndex;
+                triangles[triangleIndex++] = topLeftIndex;
+                triangles[triangleIndex++] = topRightIndex;
+
+                triangles[triangleIndex++] = bottomLeftIndex;
+                triangles[triangleIndex++] = topRightIndex;
+                triangles[triangleIndex++] = bottomRightIndex;
+            }
+        }
+
+        int lastBottomRightIndex    = vertexIndex - 1;
+        int lastTopLeftIndex        = horizontalMaximum - 1;
+        int lastBottomLeftIndex     = lastTopLeftIndex - widthPlusOne;
+        int lastTopRightIndex       = vertexIndex++;
+
+        triangles[triangleIndex++] = lastBottomLeftIndex;
+        triangles[triangleIndex++] = lastTopLeftIndex;
+        triangles[triangleIndex++] = lastTopRightIndex;
+
+        triangles[triangleIndex++] = lastBottomLeftIndex;
+        triangles[triangleIndex++] = lastTopRightIndex;
+        triangles[triangleIndex++] = lastBottomRightIndex;
+
+        vertices[lastTopRightIndex] = new Vector3(GridWidth + LineThickness, GridHeight + LineThickness);
+
+        Geometry.Clear();
+        Geometry.vertices   = vertices;
+        Geometry.triangles  = triangles;
+    }
+};
---
+++ Assets/Shaders/LineColorShader.shader
@@ -0,0 +1,95 @@
+﻿Shader "Unlit/LineColorShader"
+{
+	Properties
+	{
+		_BackgroundColor("Background Color", Color) = (1, 1, 1, 1)
+		_ColorStep("Color Step", Range(0.0, 180.0))	= 0.0
+	}
+
+	/*
+		A Shader can contain one or more SubShaders, which are primarily used to implement shaders
+		for different GPU capabilities.
+		
+		All the SubShaders should present similar results using different techniques for each
+		architecture. ShaderLab translates the code of the shader automatically to other
+		architectures.
+	*/
+	
+	SubShader
+	{
+		/*
+			Tags are a way of telling Unity certain properties of the shader we are writing.
+			For instance, the order in which it should be rendered (Queue) and how it should
+			be rendered (RenderType).
+
+			When rendering triangles, the GPU usually sorts them according to their distance
+			from the camera, so that the further ones are drawn first. This is typically enough
+			to render solid geometries, but it often fails with transparent objects.
+			
+			This is why Unity allows to specify the tag Queue which gives control on the rendering
+			order of each material. Queue accepts integer positive numbers (the smaller it is, the
+			sooner is drawn); mnemonic labels can also be used:
+
+				Background (1000): used for backgrounds and skyboxes,
+
+				Geometry (2000): the default label used for most solid objects,
+
+				Transparent (3000): used for materials with transparent properties, such glass, fire, particles and water;
+
+				Overlay (4000): used for effects such as lens flares, GUI elements and texts.
+		*/
+
+		Tags
+		{ 
+			"RenderType"	= "Opaque"
+			"Queue"			= "Geometry"
+		}
+
+		/*
+			Each SubShader is composed of a number of passes, and each Pass represents an execution
+			of the Vertex and Fragment code for the same object rendered with the Material of the Shader.
+		*/
+		
+		Pass
+		{
+			/*
+				These directives define in ShaderLab the language used for the shader. Unity can work with the
+				shader languages of Cg and GLSL.the Cg language is recommended, because several optimization
+				steps are implemented for the different architectures.
+			*/
+
+			CGPROGRAM
+			#pragma vertex		vertexShader	// compile function vertexShader() as the vertex shader.
+			#pragma fragment	fragmentShader	// compile function fragmentShader() as the fragment shader.
+
+			struct Vertex
+			{
+				float4 position : POSITION;
+			};
+
+			struct ScreenVertex
+			{
+				float4 position : SV_POSITION;
+			};
+
+			ScreenVertex vertexShader(Vertex input)
+			{
+				ScreenVertex output;
+				output.position = UnityObjectToClipPos(input.position); // mul(UNITY_MATRIX_MVP, input.position); - Removed by ShaderLab compiler
+				return output;
+			}
+
+			half4 _BackgroundColor;
+			float _ColorStep;
+			float value = 0.0;
+
+			half4 fragmentShader() : COLOR
+			{
+				value += _Time.x * _ColorStep;
+				float sine = clamp(sin(value), 0.0, 1.0);
+				return half4(_BackgroundColor.x * sine, _BackgroundColor.y * sine, _BackgroundColor.z * sine, _BackgroundColor.w);
+			}
+			ENDCG
+		}
+	}
+}
